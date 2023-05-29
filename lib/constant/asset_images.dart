b882228435927278d7121f3f30d28871a259579a
Shadow 07 <sidhuram365@gmail.com>
2023-05-29 20:09:59 +0530

    added native app icon

--- lib/constant/asset_images.dart
+++ lib/constant/asset_images.dart
@@ -1,3 +1,3 @@
-const aiImage = 'assets/images/ai-bot.png';
-const personImage = 'assets/images/person.png';
+// const aiImage = 'assets/images/ai-bot.png';
+// const personImage = 'assets/images/person.png';
 const aiSvgImage = 'assets/images/svg/ai-bot.svg';
